92197460af9f9b40ab92d6a828a9f4fcfeae0582
Drusketh <33267962+Drusketh@users.noreply.github.com>
2025-03-09 18:20:53 -0700

    Footer links added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
     <!-- About Section with Animated Gradient Background -->
     <section id="about" class="h-screen flex items-center justify-center animate-gradient-x relative overflow-hidden">
         <div class="text-center z-10">
-            <h1 class="text-4xl md:text-6xl font-bold animate-fade-in bg-clip-text">Welcome to My Portfolio</h1>
-            <p class="mt-4 text-lg animate-fade-in">I'm Nathan Pollaro, a junior engineering associate with a passion for learning, CAD, and engineering projects.</p>
+            <h1 class="text-4xl md:text-6xl font-bold animate-fade-in bg-clip-text">Welcome</h1>
+            <p class="mt-4 text-lg animate-fade-in">I'm Nathan Pollaro, a junior engineering associate with a passion for CAD and Engineering.</p>
         </div>
         <div class="absolute inset-0 bg-[url('../../public/images/background.jpg')] bg-no-repeat bg-center bg-cover bg-fixed opacity-20"></div>
     </section>
@@ -46,10 +46,23 @@
     </section>
 
     <!-- Contact Section with Gradient Animation and Scrolling Effect -->
-    <section id="contact" class="h-screen flex items-center justify-center relative overflow-hidden">
-        <div class="text-center z-10">
-            <h2 class="text-3xl md:text-5xl font-bold animate-bounce">Get In Touch</h2>
-            <p class="mt-4 text-lg animate-bounce">Feel free to reach out for any inquiries or collaborations.</p>
+    <section id="contact" class="h-fit flex flex-col items-center justify-center relative overflow-hidden">
+        <div class="flex">
+            <div class="text-center z-10">
+                <h2 class="text-3xl md:text-5xl font-bold animate-bounce">Get In Touch</h2>
+                <p class="mt-4 text-lg animate-bounce">Feel free to reach out for any inquiries or collaborations.</p>
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <div class="flex flex-row p-12 gap-12 w-max justify-between items-center">
+                <a target="_blank" href="https://www.instagram.com/nathan.pollaro/"><img src="{{ asset('svgs/icons8-instagram.svg') }}"/></a>
+                <a target="_blank" href="https://www.facebook.com/nathan.pollaro/"><img src="{{ asset('svgs/icons8-facebook.svg') }}"/></a>
+                <a target="_blank" href="https://www.linkedin.com/in/nathan-pollaro/"><img src="{{ asset('svgs/icons8-linkedin.svg') }}"/></a>
+                <a target="_blank" href="https://github.com/Drusketh/"><img src="{{ asset('svgs/icons8-github.svg') }}"/></a>
+            </div>
+            <div class="flex text-center">
+                <span> Icons by <a target="_blank" href="https://icons8.com"> Icons8</a> </span>
+            </div>
         </div>
     </section>
 @endsection
